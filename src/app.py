d0aa7792cc9ba6f21060f03190f311c43839baad
DogFortune <dogfortune.2000@gmail.com>
2025-09-30 17:16:23 +0900

    ファイルからリンクを抽出するところまで作成

--- src/app.py
+++ src/app.py
@@ -1,8 +1,24 @@
 import argparse
 from pathlib import Path
+import re
+
+
+def extract_link(file_path: str):
+    links = []
+    # 指定したファイルからリンクを抽出します。重複はこの時点で除外しますが、ファイルをまたいだリンクの重複チェックはしない。
+    # 欲しいのはファイル名と行数とリンク
+
+    with open(file_path, "r") as f:
+        lines = f.readlines()
+        for i, line in enumerate(lines):
+            print(f"{i+1}: {line}")
+            if "http" in line:
+                links.append({"line": i + 1, "link": line})
+    return links
 
 
 def lookup_file(path: str, filter="*.md"):
+    # 指定したディレクトリから検査対象のファイルを抽出します。デフォルトはmdです。
     p = Path(path)
     files = [str(item) for item in p.rglob(filter)]
     return files
@@ -18,8 +34,6 @@ def main(args=None):
     parser = create_parser()
     parsed_args = parser.parse_args(args)
     files = lookup_file(parsed_args.src)
-    print(files)
-    print(parsed_args.src)
     return files
 
 
--- tests/test_main.py
+++ tests/test_main.py
@@ -3,6 +3,15 @@ import pytest
 from pprint import pprint as pp
 
 
+def test_extract_link():
+    file_list = app.lookup_file("tests/doc/")
+    links = app.extract_link(file_list[0])
+    assert len(links) == 1
+
+    links = app.extract_link(file_list[1])
+    assert len(links) == 2
+
+
 @pytest.mark.parametrize(
     ["path"], [pytest.param(".\\tests\\doc\\"), pytest.param("tests/doc/")]
 )
