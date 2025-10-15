1003f056013f9995a3a6017a074ab2a9d0d82565
DogFortune <dogfortune.2000@gmail.com>
2025-10-15 10:20:04 +0900

    フォーマット形式テスト作成中 refs 1

--- src/main.py
+++ src/main.py
@@ -3,6 +3,10 @@ import analyze
 import argparse
 
 
+def __format__setting(format: str):
+    raise NotImplementedError
+
+
 def create_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("src", default=os.environ.get("SRC_DIR", "."))
@@ -14,7 +18,12 @@ def main(args=None):
     parser = create_parser()
     parsed_args = parser.parse_args(args)
 
-    files = analyze.search(parsed_args.src)
+    print(parsed_args)
+
+    format = __format__setting(parsed_args.format)
+    src = parsed_args.src
+
+    files = analyze.search(src)
     links = analyze.extract_link(files)
     report_data_list = analyze.check_links(links)
 
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,24 @@
 import main
+import pytest
 
 
 def test_main():
     main.main(["tests/doc/"])
+
+
+@pytest.mark.parametrize(
+    ["format"],
+    [
+        pytest.param("CONSOLE"),
+        pytest.param("console"),
+        pytest.param("JSON"),
+        pytest.param("json"),
+    ],
+)
+def test_format_args(format: str):
+    """フォーマットテスト
+
+    :param format: _description_
+    :type format: str
+    """
+    main.main(["--format", format, "tests/doc/"])
