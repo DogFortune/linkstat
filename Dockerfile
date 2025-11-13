129fbbbc6f2e590e8f211f6faa6418609a390672
DogFortune <dogfortune.2000@gmail.com>
2025-11-13 14:07:21 +0900

    Delete Submodule

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM gcr.io/distroless/python3-debian12:latest
-
-WORKDIR /app
-COPY src/ .
-
-CMD [ "app.py" ]
--- tests/test_awesome.py
+++
@@ -1,8 +0,0 @@
-import app
-import pytest
-
-
-@pytest.mark.skip()
-def test_awesome():
-    """awesomeを使ったほぼ実環境に近いパターン"""
-    app.main(["submodules/awesome/", "--report-json", "tests/awesone.json"])
